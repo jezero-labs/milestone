0875ba7874b8dce026721fb9e48ec99d24dc3e4d
KaushikKC <kccreations1704@gmail.com>
2023-11-10 23:38:12 +0530

    Completed choose location section

--- src/components/LocationCard.tsx
+++ src/components/LocationCard.tsx
@@ -2,24 +2,37 @@ import {View, Text, ImageSourcePropType, Image, StyleSheet} from 'react-native';
 import React from 'react';
 
 type LocationProps = {
-    image: ImageSourcePropType,
-    location: string,
+  image: ImageSourcePropType;
+  location: string;
 };
 
-const LocationCard = ({image,location}: ImageSourcePropType) => {
+const LocationCard = ({image, location}: LocationProps) => {
   return (
-    <View>
+    <View style={styles.card}>
       <Image style={styles.image} source={image} />
-      <Text>{location}</Text>
+      <Text style={styles.location}>{location}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    image: {
-        height: 80,
-        width: 76,
-    }
-})
+  image: {
+    height: 80,
+    width: 76,
+    borderRadius: 12,
+  },
+  location: {
+    fontSize: 15,
+    fontWeight: '600',
+    color: '#fff',
+    position: 'absolute',
+    top: 50,
+    left: 10,
+  },
+  card: {
+    marginHorizontal: 8,
+    marginVertical: 14,
+  },
+});
 
 export default LocationCard;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,87 @@
-import {View, StyleSheet, Text} from 'react-native';
-import React from 'react';
+import {View, StyleSheet, Text, Dimensions, Image} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
 import WelcomeComponent from '../components/WelcomeComponent';
-import { ScrollView } from 'react-native-gesture-handler';
+import {FlatList, ScrollView} from 'react-native-gesture-handler';
 import LocationCard from '../components/LocationCard';
 
+const data = [
+  {
+    id: '01',
+    title: 'Travel to India',
+    para: 'My trip to China started in Beijing, where I stayed in a beautiful hotel called ...',
+    profileName: 'Leslie Alexander',
+    profile: require('../assets/images/ProfileImg.png'),
+    backgroundImg: require('../assets/images/India.png'),
+  },
+  {
+    id: '02',
+    title: 'Travel to Italy',
+    para: 'My trip to China started in Beijing, where I stayed in a beautiful hotel called ...',
+    profileName: 'Leslie Alexander',
+    profile: require('../assets/images/ProfileImg.png'),
+    backgroundImg: require('../assets/images/Italy.png'),
+  },
+  {
+    id: '03',
+    title: 'Travel to Africa',
+    para: 'My trip to China started in Beijing, where I stayed in a beautiful hotel called ...',
+    profileName: 'Leslie Alexander',
+    profile: require('../assets/images/ProfileImg.png'),
+    backgroundImg: require('../assets/images/Cover.png'),
+  },
+];
+
 const Home = () => {
+  const flatlistRef: any = useRef();
+  const screenWidth = Dimensions.get('window').width;
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      if (activeIndex === data.length - 1) {
+        flatlistRef.current.scrollToIndex({
+          index: 0,
+          animation: true,
+        });
+      } else {
+        flatlistRef.current.scrollToIndex({
+          index: activeIndex + 1,
+          animation: true,
+        });
+      }
+    }, 2000);
+
+    return () => clearInterval(interval);
+  });
+
+  const getItemLayout = (data:any, index: any) => ({
+    length: screenWidth,
+    offset: screenWidth * index, // for first image - 300 * 0 = 0pixels, 300 * 1 = 300, 300*2 = 600
+    index: index,
+});
+
+  const renderItem = ({item, index}: any) => {
+    return (
+      <View>
+        <Image source={item.backgroundImg} style={{height: 392, width: 335}} />
+      </View>
+    );
+  };
+
+  const handleScroll = (event: any) => {
+    // Get the scroll position
+    const scrollPosition = event.nativeEvent.contentOffset.x;
+    console.log({scrollPosition});
+    // Get the index of current active item
+
+    const index = scrollPosition / screenWidth;
+
+    console.log({index});
+    // Update the index
+
+    setActiveIndex(index);
+  };
+
   return (
     <View>
       <WelcomeComponent
@@ -14,13 +91,44 @@ const Home = () => {
       <View>
         <Text style={styles.subHeading}>Choose location</Text>
         <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-            <LocationCard image={require('../assets/images/india.png')} location={"India"} />
-            <LocationCard image={require('../assets/images/turkey.png')} location={"Turkey"} />
-            <LocationCard image={require('../assets/images/africa.png')} location={"Africa"} />
-            <LocationCard image={require('../assets/images/italy.png')} location={"Italy"} />
-            <LocationCard image={require('../assets/images/Cover.png')} location={"West"} />
-            <LocationCard image={require('../assets/images/Shape.png')} location={"Germany"} />
+          <LocationCard
+            image={require('../assets/images/india.png')}
+            location={'India'}
+          />
+          <LocationCard
+            image={require('../assets/images/turkey.png')}
+            location={'Turkey'}
+          />
+          <LocationCard
+            image={require('../assets/images/africa.png')}
+            location={'Africa'}
+          />
+          <LocationCard
+            image={require('../assets/images/italy.png')}
+            location={'Italy'}
+          />
+          <LocationCard
+            image={require('../assets/images/Cover.png')}
+            location={'West'}
+          />
+          <LocationCard
+            image={require('../assets/images/Shape.png')}
+            location={'Germany'}
+          />
         </ScrollView>
+      </View>
+      <View style={styles.exploreContainer}>
+        <Text style={styles.exploreTitle}>Explore Place</Text>
+        <FlatList
+          data={data}
+          ref={flatlistRef}
+          getItemLayout={getItemLayout}
+          keyExtractor={(item: any) => item.id}
+          renderItem={renderItem}
+          horizontal={true}
+          pagingEnabled={true}
+          onScroll={handleScroll}
+        />
       </View>
     </View>
   );
@@ -30,12 +138,20 @@ const styles = StyleSheet.create({
   head: {
     color: 'black',
   },
-  subHeading:{
-    fontSize:15,
-    fontWeight:'600',
+  subHeading: {
+    fontSize: 15,
+    fontWeight: '600',
     color: '#321F0E',
-    marginLeft: 20
-  }
+    marginLeft: 20,
+  },
+  exploreTitle: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#000',
+  },
+  exploreContainer: {
+    padding: 20,
+  },
 });
 
 export default Home;
