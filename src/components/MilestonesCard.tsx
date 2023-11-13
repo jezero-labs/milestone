3f1a148d951a930c582ad9c354afc4d7620e2ac8
KaushikKC <kccreations1704@gmail.com>
2023-11-13 11:34:26 +0530

    Added place detail page

---
+++ src/components/MilestonesCard.tsx
@@ -0,0 +1,65 @@
+import {View, Text, ImageSourcePropType, Image, StyleSheet} from 'react-native';
+import React from 'react';
+import {wp} from '../utils/ScreenDimension';
+
+type MilestoneProps = {
+  image: ImageSourcePropType;
+  title: string;
+  location: string;
+};
+
+const MilestonesCard = ({image, title, location}: MilestoneProps) => {
+  return (
+    <View style={styles.milsestoneCard}>
+      <Image style={styles.milesrtoneImg} source={image} />
+      <View style={styles.milestoneDiv}>
+        <View>
+          <Text style={styles.milestoneText}>{title}</Text>
+          <View style={styles.milestoneLocationDiv}>
+            <Image
+              style={styles.mapIcon}
+              source={require('../assets/images/map.png')}
+            />
+            <Text>{location}</Text>
+          </View>
+        </View>
+        <Image source={require('../assets/images/milestonedetail.png')} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  milsestoneCard: {
+    height: 214,
+    width: wp(100) - 40,
+    backgroundColor: '#fff',
+    padding: 16,
+    borderRadius: 26,
+    marginBottom: 20,
+  },
+  milesrtoneImg: {
+    width: wp(100) - 72,
+    borderRadius: 20,
+  },
+  milestoneText: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#121212',
+    marginTop: 16,
+  },
+  milestoneLocationDiv: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  mapIcon: {
+    marginRight: 4,
+  },
+  milestoneDiv: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});
+
+export default MilestonesCard;
--- src/screens/PlaceDetails.tsx
+++ src/screens/PlaceDetails.tsx
@@ -1,17 +1,141 @@
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 import React from 'react';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
+import {wp} from '../utils/ScreenDimension';
+import MilestonesCard from '../components/MilestonesCard';
 
 const PlaceDetails = () => {
   return (
-    <View>
-      <View>
-        <Image source={require('../assets/images/arrow.png')} />
-        <Text>Pondy</Text>
-        <Image source={require('../assets/images/dot.png')} />
+    <View style={styles.outterContainer}>
+      <View style={styles.topNav}>
+        <TouchableOpacity style={styles.navMenu}>
+          <Image source={require('../assets/images/arrow.png')} />
+        </TouchableOpacity>
+        <Text style={styles.navName}>Pondy</Text>
+        <TouchableOpacity style={styles.navMenu}>
+          <Image source={require('../assets/images/dot.png')} />
+        </TouchableOpacity>
       </View>
-      <Text>PlaceDetails</Text>
+      <View>
+        <Image source={require('../assets/images/pondy.png')} />
+        <View style={styles.placeTitleContainer}>
+          <View>
+            <Text style={styles.placeTitle}>Pondicherry</Text>
+            <View style={styles.locationDiv}>
+              <Image source={require('../assets/images/explore.png')} />
+              <Text style={styles.locationText}>Wayanad</Text>
+            </View>
+          </View>
+          <View style={styles.locationDiv}>
+            <Image source={require('../assets/images/share.png')} />
+            <Image
+              style={styles.shareIcon}
+              source={require('../assets/images/heart.png')}
+            />
+          </View>
+        </View>
+      </View>
+      <Text style={styles.storyText}>People Stories</Text>
+      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+        <Image
+          style={styles.storyImg}
+          source={require('../assets/images/story.png')}
+        />
+        <Image
+          style={styles.storyImg}
+          source={require('../assets/images/story0.png')}
+        />
+        <Image
+          style={styles.storyImg}
+          source={require('../assets/images/story1.png')}
+        />
+        <Image
+          style={styles.storyImg}
+          source={require('../assets/images/story2.png')}
+        />
+        <Image
+          style={styles.storyImg}
+          source={require('../assets/images/story3.png')}
+        />
+      </ScrollView>
+      <Text style={styles.milestoneText}>Milestones</Text>
+      <ScrollView>
+        <MilestonesCard
+          image={require('../assets/images/milestoneImg.png')}
+          title="Night life in Pondy"
+          location="Wayanad"
+        />
+        <MilestonesCard
+          image={require('../assets/images/milestoneImg.png')}
+          title="Night life in Pondy"
+          location="Wayanad"
+        />
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  topNav: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+  },
+  outterContainer: {
+    padding: 20,
+  },
+  navMenu: {
+    width: 40,
+    height: 40,
+    backgroundColor: '#fff',
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  navName: {
+    fontSize: 16,
+    fontWeight: '700',
+    color: '#121212',
+  },
+  placeTitleContainer: {
+    flexDirection: 'row',
+    position: 'absolute',
+    justifyContent: 'space-between',
+    top: 231,
+    width: wp(100) - 80,
+    marginHorizontal: 10,
+  },
+  placeTitle: {
+    fontSize: 26,
+    fontWeight: '500',
+    color: '#fff',
+  },
+  locationDiv: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  shareIcon: {
+    marginLeft: 10,
+  },
+  locationText: {
+    color: '#fff',
+    marginLeft: 5,
+  },
+  storyText: {
+    color: '#121212',
+    fontSize: 14,
+    marginVertical: 16,
+  },
+  storyImg: {
+    marginRight: 10,
+  },
+  milestoneText: {
+    fontSize: 16,
+    color: '#121212',
+    marginVertical: 16,
+    fontWeight: '600',
+  },
+});
 
 export default PlaceDetails;
