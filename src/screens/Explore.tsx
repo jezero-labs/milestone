b8ce59f00d167766247a9a4742f03a37c59ef58a
KaushikKC <kccreations1704@gmail.com>
2023-11-13 15:07:06 +0530

    Completed Explore page

--- src/screens/Explore.tsx
+++ src/screens/Explore.tsx
@@ -1,12 +1,141 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
+import React, {useState} from 'react';
+import WelcomeComponent from '../components/WelcomeComponent';
+import MilestonesCard from '../components/MilestonesCard';
 
-const Explore = () => {
+const Explore = ({navigation}: any) => {
+  const [locationCategory, setLocationCategory] = useState<string>('All');
   return (
     <View>
-      <Text>Explore</Text>
+      <WelcomeComponent
+        name={'Kaushik KC'}
+        image={require('../assets/images/ProfileImg.png')}
+      />
+      <View style={styles.exploreDiv}>
+        <Text style={styles.exploreHeading}>Incredible India</Text>
+        <Text style={styles.exploreSubHeadinhDark}>
+          <Text style={styles.exploreSubHeading}>Let’s Explore </Text>
+          Milestones
+        </Text>
+        <ScrollView
+          style={styles.locationCategory}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}>
+          <TouchableOpacity onPress={() => setLocationCategory('All')}>
+            <Text
+              style={
+                locationCategory === 'All'
+                  ? styles.locationCategoryTextActive
+                  : styles.locationCategoryText
+              }>
+              All
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setLocationCategory('Popular')}>
+            <Text
+              style={
+                locationCategory === 'Popular'
+                  ? styles.locationCategoryTextActive
+                  : styles.locationCategoryText
+              }>
+              Popular
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setLocationCategory('Nearby')}>
+            <Text
+              style={
+                locationCategory === 'Nearby'
+                  ? styles.locationCategoryTextActive
+                  : styles.locationCategoryText
+              }>
+              Nearby
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setLocationCategory('Recomended')}>
+            <Text
+              style={
+                locationCategory === 'Recomended'
+                  ? styles.locationCategoryTextActive
+                  : styles.locationCategoryText
+              }>
+              Recomended
+            </Text>
+          </TouchableOpacity>
+        </ScrollView>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <MilestonesCard
+            image={require('../assets/images/milestoneImg.png')}
+            title="Night life in Pondy"
+            location="Wayanad"
+            navigation={navigation}
+          />
+          <MilestonesCard
+            image={require('../assets/images/milestoneImg.png')}
+            title="Night life in Pondy"
+            location="Wayanad"
+            navigation={navigation}
+          />
+          <MilestonesCard
+            image={require('../assets/images/milestoneImg.png')}
+            title="Night life in Pondy"
+            location="Wayanad"
+            navigation={navigation}
+          />
+          <MilestonesCard
+            image={require('../assets/images/milestoneImg.png')}
+            title="Night life in Pondy"
+            location="Wayanad"
+            navigation={navigation}
+          />
+        </ScrollView>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  exploreDiv: {
+    paddingHorizontal: 20,
+  },
+  locationCategory: {
+    flexDirection: 'row',
+    marginVertical: 16,
+  },
+  locationCategoryText: {
+    fontSize: 14,
+    color: '#121212',
+    fontWeight: '500',
+    marginRight: 40,
+  },
+  locationCategoryTextActive: {
+    fontSize: 14,
+    color: '#F36D72',
+    fontWeight: '500',
+    marginRight: 40,
+    borderBottomColor: '#F36D72',
+    borderBottomWidth: 2,
+  },
+  exploreHeading: {
+    fontSize: 25,
+    color: '#121212',
+    fontWeight: '800',
+  },
+  exploreSubHeading: {
+    fontSize: 22,
+    color: '#6F7789',
+    fontWeight: '600',
+  },
+  exploreSubHeadinhDark: {
+    fontSize: 22,
+    color: '#121212',
+    fontWeight: '600',
+  },
+});
 
 export default Explore;
--- src/screens/Marketplace.tsx
+++ src/screens/Marketplace.tsx
@@ -1,12 +1,41 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
+import WelcomeComponent from '../components/WelcomeComponent';
+import {wp} from '../utils/ScreenDimension';
 
 const Marketplace = () => {
   return (
     <View>
-      <Text>Marketplace</Text>
+      <WelcomeComponent
+        name={'Kaushik KC'}
+        image={require('../assets/images/ProfileImg.png')}
+      />
+      <View style={styles.marketplaceDiv}>
+        <Text style={styles.marketplaceText}>Marketplace</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  marketplaceText: {
+    flexDirection: 'column',
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#121212',
+    fontSize: 18,
+  },
+  marketplaceDiv: {
+    flexDirection: 'column',
+    width: wp(100),
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: wp(100) - 240,
+    marginVertical: 260,
+  },
+});
 
 export default Marketplace;
